7c6fdc4fd0a1cf4a550e29b6af170fadde32f36f
Anabil Debnath <73880095+anabildebnath@users.noreply.github.com>
2021-09-21 21:04:57 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,3 @@ function changecolor(color){
     const circle= document.querySelector('.circle');
     circle.style.background=color;
 }
-
-exports.default = series(scssTask, jsTask, broserSyncServe, watchTask);
-exports.build = series(scssTask,jsTask);
