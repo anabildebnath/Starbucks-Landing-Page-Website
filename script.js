a0f96d8dd2f9acdfcc11ca359c1b98535bbe5375
Anabil Debnath <73880095+anabildebnath@users.noreply.github.com>
2021-09-21 09:56:52 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgselector(anything) {
+  document.querySelector(".starbucks").src = anything;
+}
+
+function changecolor(color){
+    const circle= document.querySelector('.circle');
+    circle.style.background=color;
+}
